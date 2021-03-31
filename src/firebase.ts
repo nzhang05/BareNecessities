f2d35b7882dab9c4bed5ea7e246a55984857430b
keisha mukasa <keisha.mukasa@gmail.com>
2021-03-31 00:14:28 -0400

    Changed eslint rules and created firebase methods file

---
+++ src/firebase.ts
@@ -0,0 +1,65 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import * as admin from 'firebase-admin';
+import * as env from 'env-var';
+// eslint-disable-next-line no-multi-str
+import serviceAccount from '\
+./bare-necessities-60939-firebase-adminsdk-1vgof-20badddf68.json';
+
+// firebase client
+const firebaseConfig = {
+  apiKey: `${env.get('FIREBASE_API_KEY').asString()}`,
+  authDomain: `${env.get('FIREBASE_PROJECT_ID').asString()}.firebaseapp.com`,
+  databaseURL: `${env.get('FIREBASE_URL').asString()}`,
+  projectId: `${env.get('FIREBASE_PROJECT_ID').asString()}`,
+  storageBucket: `${env.get('FIREBASE_PROJECT_ID').asString()}.appspot.com`,
+  messagingSenderId: env.get('FIREBASE_MESSAGING_SENDER').asString(),
+  appId: env.get('FIREBASE_APP_ID').asString(),
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const params = {
+  type: serviceAccount.type,
+  projectId: serviceAccount.project_id,
+  privateKeyId: serviceAccount.private_key_id,
+  privateKey: serviceAccount.private_key,
+  clientEmail: serviceAccount.client_email,
+  clientId: serviceAccount.client_id,
+  authUri: serviceAccount.auth_uri,
+  tokenUri: serviceAccount.token_uri,
+  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(params),
+  databaseURL: 'https://bare-necessities-60939-default-rtdb.firebaseio.com',
+});
+
+const adminDb = admin.database();
+
+export const authenticateUser = async (email: string, password: string) => {
+  let error = true;
+  const success = await firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      error = true;
+      return true;
+    })
+    .catch(() => {
+      error = false;
+      return false;
+    });
+  firebase.auth()
+    .setPersistence(firebase.auth.Auth.Persistence.NONE);
+  return error && success;
+};
+
+export const signOutUser = () => firebase.auth().signOut();
+
+export const getStoreProducts = async (storeName: string) => adminDb.ref(`Stores/${storeName}/Products`)
+  .once('value')
+  .then((snapshot) => snapshot.val());
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
 /* eslint-disable no-console */
-// import twilio from 'twilio';
 import express from 'express';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
 import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
 import * as env from 'env-var';
-// import messages from './messages/messages.json';
+import { getStoreProducts } from './firebase.ts';
 
 require('dotenv').config();
 
 const expressServerPort = env.get('PORT').asString();
-const firebaseConfig = {
-  apiKey: `${env.get('FIREBASE_API_KEY').asString()}`,
-  authDomain: `${env.get('FIREBASE_PROJECT_ID').asString()}.firebaseapp.com`,
-  databaseURL: `${env.get('FIREBASE_URL').asString()}`,
-  projectId: `${env.get('FIREBASE_PROJECT_ID').asString()}`,
-  storageBucket: `${env.get('FIREBASE_PROJECT_ID').asString()}.appspot.com`,
-  messagingSenderId: env.get('FIREBASE_MESSAGING_SENDER').asString(),
-  appId: env.get('FIREBASE_APP_ID').asString(),
-};
 
 console.log(
   'Your environment variable TWILIO_ACCOUNT_SID has the value: ',
@@ -50,70 +37,7 @@ app.listen(expressServerPort, () => {
   console.log(`Listening at http://localhost:${expressServerPort}/`);
 });
 
-// const numbers = {
-//   twilio: `+${process.env.TWILIO_TEST_NUMBER}`,
-//   andrew: `+${process.env.ANDREW_CELL_NUMBER}`,
-// };
-
-// create a message -- commented out in order not waste money during testing
-// eslint-disable-next-line new-cap
-// const twilioClient = twilio(
-//   process.env.TWILIO_ACCOUNT_SID,
-//   process.env.TWILIO_AUTH_TOKEN,
-// );
-// twilioClient.messages.create(
-//   {
-//     to: numbers.andrew,
-//     from: numbers.twilio,
-//     body: messages.greeting,
-//   },
-//   (err, data) => {
-//     console.log('ERROR:', { err, data });
-//   },
-// );
-
-// firebase client
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
-const authenticateUser = async (email: string, password: string) => {
-  let error = true;
-  const success = await firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(() => {
-      error = true;
-      return true;
-    })
-    .catch((err) => {
-      console.log(err);
-      error = false;
-      return false;
-    });
-  // firebase.auth()
-  //   .setPersistence(firebase.auth.Auth.Persistence.SESSION);
-  return error && success;
-};
-
-const signOutUser = () => {
-  firebase.auth().signOut();
-};
-
-const email = env.get('ANDREW_GMAIL').asString();
-const password = env.get('ANDREW_PASSWORD').asString();
-
-if (email && password) {
-  authenticateUser(email, password)
-    .then(() => {
-      db.ref('Numbers')
-        .once('value')
-        .then((snapshot) => {
-          const nums = snapshot.val();
-          console.log(nums);
-        });
-      signOutUser();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+const storeName = 'KeishaFarm';
+getStoreProducts(storeName).then((products) => {
+  console.log(products);
+});
