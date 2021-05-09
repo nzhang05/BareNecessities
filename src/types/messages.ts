5e0430531e801554bfdc88b94b0ff5b238b4f95c
agross09 <aleogross@gmail.com>
2021-05-08 20:47:49 -0400

    New Creator and StoreData types plus edits to TreState type

--- src/types/messages.ts
+++ src/types/messages.ts
@@ -1,8 +1,32 @@
+export interface Creator {
+  id: number;
+  // eslint-disable-next-line camelcase
+  first_name: string;
+  // eslint-disable-next-line camelcase
+  last_name: string;
+  phone: string;
+  email: string;
+  // eslint-disable-next-line camelcase
+  farm_distance: string;
+}
+
+export interface StoreData {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  currency: string;
+  locality: string;
+  creator: Creator;
+}
+
 export interface TreeState {
   location: string;
-  userStatus: string;
-  existingVendor: boolean;
+  product: string;
   storeName: string;
+  storeData: StoreData[];
+  counter: number;
+  counterDelta: 0 | -1 | -2 | -3;
 }
 
 export interface MessageObject {
