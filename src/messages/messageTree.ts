4a36defbd4cd4b25731fede104369b0193f6b783
agross09 <aleogross@gmail.com>
2021-05-09 01:02:06 -0400

    Replaced original implementation of messageTree.ts sto eliminate vendor side and connect buyer side to Mkulima Market API

--- src/messages/messageTree.ts
+++ src/messages/messageTree.ts
@@ -1,169 +1,237 @@
+import _ from 'lodash';
 import messages from './messages.json';
 // eslint-disable-next-line import/extensions
-import { TreeState, MessageObject } from '../types/messages';
-import * as firebaseLib from '../firebase';
+import {
+  TreeState, MessageObject, StoreData, ProductDetails, Creator,
+} from '../types/messages';
+import { getFarmerInfo, getFarmersWithProduct } from '../mkulimaRequests';
 
-const startMessage = (
+const keyWords = {
+  search: 'search',
+  more: 'more',
+  exit: 'exit',
+  back: 'back',
+  new: 'new',
+};
+
+/**
+ * Helper functions
+ */
+
+const enumerateStringList = (l: string[]) => {
+  const nums = [...Array(l.length + 1).keys()]
+    .slice(1)
+    .map((n) => `${n.toString(10)}.`);
+  return _.zip(nums, l)
+    .map((p) => p.join(' '));
+};
+
+const formatVendorString = (vendors: StoreData[]): string[] => {
+  const topVendors = vendors.map((vendor) => {
+    const name = vendor.creator.first_name && vendor.creator.last_name
+      ? `${vendor.creator.first_name} ${vendor.creator.last_name}`
+      : vendor.creator.full_names;
+    return `${name.trim()}, ${vendor.locality}`;
+  });
+  return enumerateStringList(topVendors);
+};
+
+const formatVendorDetails = (
+  creator: Creator,
+  productDetailsList: ProductDetails[],
+): string => {
+  const { locality } = productDetailsList[0];
+  const name = creator.first_name && creator.last_name
+    ? `${creator.first_name} ${creator.last_name}`
+    : creator.full_names;
+  const creatorString = `${name.trim()}\n${locality}\n${creator.phone}\n`
+    + `${creator.email}`;
+  const productsStringList = productDetailsList.map((product) =>
+    `${product.title} ${product.price} ${product.currency}\n`);
+  const enumeratedProducts = enumerateStringList(productsStringList);
+
+  return `${creatorString}\n\n${enumeratedProducts.join('')}`;
+};
+
+/**
+ * Buyer Tree functions
+ */
+
+const startMessage = async (
   _userInput: string,
   treeState: TreeState,
-): MessageObject =>
+): Promise<MessageObject> =>
   ({ message: messages.initResponse, treeState });
 
-const checkBuyerVendor = (
-  userInput: string,
-  treeState: TreeState,
-): MessageObject => {
-  const newTreeState: TreeState = treeState;
-  if (userInput === '1' || userInput === 'one') {
-    newTreeState.userStatus = 'buyer';
-    return { message: messages.isBuyerResponse, treeState: newTreeState };
-  }
-  if (userInput === '2' || userInput === 'two') {
-    newTreeState.userStatus = 'vendor';
-    return { message: messages.checkExistingVendor, treeState: newTreeState };
-  }
-  return { message: messages.unrecognizedResponse, treeState: newTreeState };
-};
-
-const storeLocation = (
-  userInput: string,
-  treeState: TreeState,
-): MessageObject => {
-  const newTreeState: TreeState = treeState;
-  newTreeState.location = userInput;
-  return {
-    message: messages.listProductsInstructions,
-    treeState: newTreeState,
-  };
-};
-
-const getProducts = (
-  _userInput: string,
-  treeState: TreeState,
-): MessageObject =>
-  ({ message: messages.listMatchingVendors, treeState });
-
-const selectVendor = (
-  _userInput: string,
-  treeState: TreeState,
-): MessageObject => ({ message: messages.choseAndrewFarm, treeState });
-
-const searchOrQuit = (
-  _userInput: string,
-  treeState: TreeState,
-): MessageObject => ({ message: messages.newSearchOrQuit, treeState });
-
-const exitService = (
-  _userInput: string,
-  treeState: TreeState,
-): MessageObject => ({ message: messages.exit, treeState });
-
-const createNewVendor = (
-  userInput: string,
-  treeState: TreeState,
-): MessageObject => {
-  let newMessage = '';
-  const newTreeState: TreeState = treeState;
-  if (userInput === 'n' || userInput === 'no') {
-    newMessage = messages.storeRegisteration;
-    newTreeState.existingVendor = false;
-  } else {
-    newMessage = messages.existingVendor;
-    newTreeState.existingVendor = true;
-  }
-  return { message: newMessage, treeState: newTreeState };
-};
-
-const existingVendorCheck = async (storeName: string): Promise<boolean> =>
-  firebaseLib.getStoreNames().then((storeNames) => storeName in storeNames);
-
-const getVendorDetailsMessage = async (storeName: string): Promise<string> =>
-  firebaseLib.getStoreProducts(storeName).then((products) => {
-  // [ProductNameKey, {Price, Quantity, Unit}]
-    const productString = Object.entries(products).map((productPair) => {
-      const productName = productPair[0];
-      const productDetails: any = productPair[1];
-
-      if (productDetails.Quantity && productDetails.Unit) {
-      // RETURN SHAPE: \n<productName> <productQuantity><productUnit>\n[]
-        return `${productName} `
-          + `${productDetails.Quantity}${productDetails.Unit}\n`;
-      }
-      return `ERROR: No store details found for ${storeName}`;
-    });
-    return `\n${productString}`;
-  });
-
-const showVendorDetailsOrRegister = (
+const searchMessage = async (
   userInput: string,
   treeState: TreeState,
 ): Promise<MessageObject> => {
-  // assuming this data comes in the right order -- TODO: write checks for data
-  // <store name>, <phone>, <email>, <street_address>, <city>
+  // const queryList = userInput.split(',').map((w) => w.trim());
+  const queryListWithCommand = userInput.split(' ');
+  const command = queryListWithCommand[0];
+  const queryListWithOutCommand = queryListWithCommand
+    .slice(1)
+    .map((q) => q.trim())
+    .filter((q) => q !== ' ')
+    .join(' ')
+    .split(',')
+    .map((w) => w.trim());
 
-  const newTreeState: TreeState = treeState;
+  const queryList = [command].concat(queryListWithOutCommand);
 
-  return existingVendorCheck(userInput).then((vendorExists) => {
-    if (treeState.existingVendor && vendorExists) {
-      newTreeState.storeName = userInput;
-      return getVendorDetailsMessage(userInput)
-        .then((vendorDetailsMessage) => ({
-          message: messages.showVendorDetails
-            + vendorDetailsMessage
-            + messages.showVendorOperations,
-          treeState: newTreeState,
-        }));
-    }
-    const detailList = userInput.split(',').map((detail) => detail.trim());
+  if (!(queryList[0] in keyWords)) {
+    // format error message
+    return {
+      message: messages.unrecognizedResponse,
+      treeState: { ...treeState, counterDelta: -1 },
+    };
+  }
 
-    if (detailList.length !== 5) {
-      // change this to be a message that indicates bad input
-      return { message: messages.unrecognizedResponse, treeState };
-    }
-    const storeName = detailList[0];
-    const storePhone = detailList[1];
-    const storeEmail = detailList[2];
-    const storeAddress = detailList[3];
-    const storeCity = detailList[4];
-    const location = firebaseLib.createStoreLocation(
-      storeAddress,
-      storeCity,
-    );
-    const contactInfo = firebaseLib.createContactInfo(
-      location,
-      storePhone,
-      storeEmail,
-    );
+  switch (queryList[0]) {
+    case keyWords.search:
+      // eslint-disable-next-line no-constant-condition
+      if (queryList.length === 3) {
+        const newTreeState: TreeState = treeState;
+        const vendors = await getFarmersWithProduct(queryList[1], queryList[2]);
+        const topVendors = vendors.slice(0, 5);
+        const enumeratedVendors = formatVendorString(topVendors);
+        const message = enumeratedVendors.length === 0
+          ? messages.searchResultsResponse + messages.noVendorsFound
+          : `${messages.searchResultsResponse
+          }\n${enumeratedVendors.join('\n')}\n\n${
+            messages.searchResultsInstructions}`;
 
-    return firebaseLib.createStore(storeName, contactInfo).then((success) => {
-      if (!success) {
-        return { message: messages.unrecognizedResponse, treeState };
+        newTreeState.storeData = topVendors;
+        newTreeState.enumeratedVendors = enumeratedVendors;
+
+        return { message, treeState: newTreeState };
       }
-      return getVendorDetailsMessage(userInput)
-        .then((vendorDetailsMessage) => ({
-          message: messages.showVendorDetails
-                + vendorDetailsMessage
-                + messages.showVendorOperations,
-          treeState: newTreeState,
-        }));
-    });
-  });
+      return {
+        message: messages.unrecognizedResponse,
+        treeState: { ...treeState, counterDelta: -1 },
+      };
+    case keyWords.exit:
+      return { message: messages.exit, treeState };
+    default:
+      return {
+        message: messages.unrecognizedResponse,
+        treeState: { ...treeState, counterDelta: -1 },
+      };
+  }
 };
 
+const selectVendorMessage = async (
+  userInput: string,
+  treeState: TreeState,
+): Promise<MessageObject> => {
+  const queryList = userInput
+    .split(' ')
+    .filter((q) => q !== ' ' && q !== '')
+    .map((w) => w.trim());
+
+  if (!queryList[0] || !(queryList[0] in keyWords)) {
+    // format error message
+    return {
+      message: messages.unrecognizedResponse,
+      treeState: { ...treeState, counterDelta: -1 },
+    };
+  }
+
+  switch (queryList[0]) {
+    case keyWords.more:
+      if (queryList.length === 2) {
+        const vendorNum: number = Number(queryList[1]);
+        if (vendorNum > treeState.storeData.length) {
+          return {
+            message: messages.unrecognizedResponse,
+            treeState: { ...treeState, counterDelta: -1 },
+          };
+        }
+        const productOfVendor: StoreData = treeState.storeData[vendorNum - 1];
+        const vendorDetails: ProductDetails[] = await getFarmerInfo(
+          productOfVendor.creator.id,
+        );
+        const selectedVendorResponse = formatVendorDetails(
+          productOfVendor.creator,
+          vendorDetails,
+        );
+        return {
+          message: `${selectedVendorResponse}\n${messages.resetMessage}`,
+          treeState,
+        };
+      }
+      return {
+        message: messages.unrecognizedResponse,
+        treeState: { ...treeState, counterDelta: -1 },
+      };
+    case keyWords.new:
+      // bring back search step (i.e. delta -= 3)
+      return {
+        message: messages.initResponse,
+        treeState: { ...treeState, counterDelta: -2 },
+      };
+    case keyWords.back:
+      // populate this message with data from treeState
+      return {
+        message: messages.initResponse,
+        treeState: { ...treeState, counterDelta: -2 },
+      };
+    case keyWords.exit:
+      return { message: messages.exit, treeState };
+    default:
+      return {
+        message: messages.unrecognizedResponse,
+        treeState: { ...treeState, counterDelta: -1 },
+      };
+  }
+};
+
+const resetMessage = async (
+  userInput: string,
+  treeState: TreeState,
+): Promise<MessageObject> => {
+  const queryList = userInput.split(',').map((w) => w.trim());
+  if (queryList.length !== 1 || !(queryList[0] in keyWords)) {
+    // format error message
+    return { message: messages.unrecognizedResponse, treeState };
+  }
+
+  switch (queryList[0]) {
+    case keyWords.new:
+      // bring back search step (i.e. delta -= 3)
+      return {
+        message: messages.initResponse,
+        treeState: { ...treeState, counterDelta: -3 },
+      };
+    case keyWords.back:
+      // bring back step (i.e. delta -= 2)
+      return {
+        message: `${messages.searchResultsResponse}\n${
+          treeState.enumeratedVendors.join('\n')}\n\n${
+          messages.searchResultsInstructions}`,
+        treeState: { ...treeState, counterDelta: -2 },
+      };
+    case keyWords.exit:
+      return { message: messages.exit, treeState };
+    default:
+      return {
+        message: messages.unrecognizedResponse,
+        treeState: { ...treeState, counterDelta: -1 },
+      };
+  }
+};
+
+const exitService = async (
+  _userInput: string,
+  treeState: TreeState,
+): Promise<MessageObject> => ({ message: messages.exit, treeState });
+
+// eslint-disable-next-line import/prefer-default-export
 export const buyerMessageTree = [
   startMessage,
-  checkBuyerVendor,
-  storeLocation,
-  getProducts,
-  selectVendor,
-  searchOrQuit,
-  exitService,
-];
-
-export const vendorMessageTree = [
-  startMessage,
-  checkBuyerVendor,
-  createNewVendor,
-  showVendorDetailsOrRegister,
+  searchMessage,
+  selectVendorMessage,
+  resetMessage,
   exitService,
 ];
